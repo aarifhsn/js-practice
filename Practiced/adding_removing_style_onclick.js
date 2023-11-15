7612cdbf99f2c01abb8c880ef7d8db77352db586
Md. Arif Hassan <aarifhsn@gmail.com>
2023-11-15 16:37:29 +0600

    Create adding_removing_style_onclick.js

---
+++ Practiced/adding_removing_style_onclick.js
@@ -0,0 +1,9 @@
+let add_style = document.querySelector("#styleChange"); 
+
+function addStyle() {
+    add_style.classList.add("para_add");
+}
+
+function removeStyle() {
+    add_style.classList.remove("para_add");
+}
